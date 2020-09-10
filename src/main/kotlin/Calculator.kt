cdf8f967a29cac2d3f5aeaa36f8d3e4c6e55f70d
heavyrain <yessss42@gmail.com>
2020-09-10 14:53:00 +0300

    all methods used

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -2,5 +2,7 @@ class Calculator {
     fun main(){
         val newTable: Table = Table(4,67)
         newTable.printAuthor("Артем")
+        newTable.printDifference()
+        newTable.printArea()
     }
 }
